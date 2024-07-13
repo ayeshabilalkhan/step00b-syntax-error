f2d94edbdee8d4266ecc59d096c28e67053e6519
Ayesha Bilal Khan <160850914+ayeshabilalkhan@users.noreply.github.com>
2024-07-13 23:00:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// Syntax Error
+// lett message = "Hello World";//syntax error
+// console.log(message);
+VarDatfunction;
+greet(name, string);
+{
+    console.log("Hello, " + name);
+}
+greet("Alice"); // Function call with correct argument
+// Syntax error: Missing closing parenthesis
+greet("Bob"); // <-- Incorrect syntax, semicolon instead of closing parenthesis
---
+++ app.ts
@@ -0,0 +1,7 @@
+// Syntax Error
+lett message = "Hello World";//syntax error
+console.log(message);
+
+// correct version
+let message = "Hello World";
+console.log(message);
